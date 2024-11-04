f7af65ab46746038a3db56141b9f6a025fb847aa
Sidinei Da Rosa <70112853+SidineiDaRosa@users.noreply.github.com>
2024-11-04 16:43:36 -0300

    up

--- resources/views/app/check_list/index.blade.php
+++ resources/views/app/check_list/index.blade.php
@@ -127,7 +127,7 @@
                     $horasDiferenca = ($diferenca->days * 24); // Converte os dias para horas
 
                     // Defina o intervalo de verificação em horas (360hs no seu caso)
-                    $intervaloVerificacao = 360;
+                    $intervaloVerificacao = 330;
                     @endphp
 
                     <!-- Para depuração: exibe a diferença em horas -->
