48c41c385806b3648d5d2fba14caefea51c49655
Sidinei Da Rosa <70112853+SidineiDaRosa@users.noreply.github.com>
2025-05-31 07:18:28 -0300

    up

--- app/Http/Controllers/EquipamentoController.php
+++ app/Http/Controllers/EquipamentoController.php
@@ -255,7 +255,7 @@ class EquipamentoController extends Controller
             $hr_old = $equipamento->horimetro;
             // Atualiza o valor do horímetroadd .
 
-            $equipamento->horimetro =  $request->horimetro;
+            $equipamento->horimetro =  $hr_old + ($request->horimetro / 60);
             $equipamento->save();
 
             // Retorna uma resposta JSON com sucesso
