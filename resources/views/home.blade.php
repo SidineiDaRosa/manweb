624f9e1cd7fc544ec3b034645aee816248f79c91
Seu Nome <seu-email@exemplo.com>
2025-07-19 12:27:06 -0300

    ptd

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<!--pagina-->
+<!--pagina alterada-->
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
