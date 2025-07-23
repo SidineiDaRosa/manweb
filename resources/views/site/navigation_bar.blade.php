3cc9223fcf3aa5e25a9f56afeaafeaa982ba9fa0
Sidinei Da Rosa <70112853+SidineiDaRosa@users.noreply.github.com>
2025-07-23 17:04:36 -0300

    udp

--- resources/views/site/navigation_bar.blade.php
+++ resources/views/site/navigation_bar.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon" />
     <link rel="stylesheet" href="{{ asset('css/icofont.min.css') }}">
     <style>
         body {
