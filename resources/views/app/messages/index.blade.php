dd8d0e5c2220054bcaa06a21c3917b1f132a9a76
Sidinei Da Rosa <70112853+SidineiDaRosa@users.noreply.github.com>
2025-07-25 09:18:54 -0300

    upd

--- resources/views/app/messages/index.blade.php
+++ resources/views/app/messages/index.blade.php
@@ -1,9 +1,14 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
-
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
     <style>
         body {
             font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
