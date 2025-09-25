752274f17d2c89ab46adec7928739b5538a99ad6
Sidinei Da Rosa <70112853+SidineiDaRosa@users.noreply.github.com>
2025-09-25 09:22:44 -0300

    up

--- resources/views/app/produto/produto_aplicacao.blade.php
+++ resources/views/app/produto/produto_aplicacao.blade.php
@@ -1,73 +1,61 @@
 @extends('app.layouts.app')
+
 <main class="content">
     <div class="card">
-        <div class="card-header-template">
+        <div class="card-header d-flex justify-content-between align-items-center">
             <div>
-                Onde o produto é aplicado
+                Onde o produto é aplicado esse componente
             </div>
-            <a class="btn btn-outline-dark sm" href="{{ route('app.home') }}">
-                <i class="icofont-dashboard"></i> dashboard
-            </a>
-            <a href="{{ route('produto.index') }}" class="btn btn-outline-primary btn-sm">
-                    <span class="material-symbols-outlined">
-                        format_list_bulleted
-                    </span>
+            <div class="d-flex gap-2">
+                <a class="btn btn-outline-dark btn-sm" href="{{ route('app.home') }}">
+                    <i class="icofont-dashboard"></i> Dashboard
                 </a>
+                <a href="{{ route('produto.index') }}" class="btn btn-outline-primary btn-sm">
+                    Produto
+                </a>
+            </div>
         </div>
-        <style>
-            .table_produtos {
-                height: 40px;
-                border: solid 1px;
 
-            }
+        <div class="table-responsive">
+            <table class="table table-bordered table-striped table-hover align-middle">
+                <thead class="table-dark text-center">
+                    <tr>
+                        <th>ID do Produto</th>
+                        <th>Nome do Produto</th>
+                        <th>Nome do Equipamento</th>
+                        <th>Quantidade</th>
+                        <th>Descrição da Aplicação</th>
+                        <th>Horas Próxima Manutenção</th>
+                        <th>Data Próxima Manutenção</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($produto_aplicacao as $produto_aplicacao_f)
+                        <tr>
+                            <td class="text-center">{{ $produto_aplicacao_f->produto_id }}</td>
+                            
+                            @php
+                                $produto = $produtos->firstWhere('id', $produto_aplicacao_f->produto_id);
+                            @endphp
+                            <td>
+                                {{ $produto ? $produto->nome : 'Produto não encontrado' }}
+                            </td>
 
-            .table_produtos td {
-                border: solid 1px;
-                height:30px;
-            }
-        </style>
-        <table class="table_produtos">
-            <thead>
-                <tr>
-                    <th>ID do Produto</th>
-                    <th>Nome do Produto</th>
-                    <th>Nome do Equipamento</th>
-                    <th>Quantidade</th>
-                    <th>Descrição da Aplicação</th>
-                    <th>Horas Próxima Manutenção</th>
-                    <th>Data Próxima Manutenção</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($produto_aplicacao as $produto_aplicacao_f)
-                <tr>
-                    <td>{{ $produto_aplicacao_f->produto_id }}</td>
-                    @php
-                    $produto = $produtos->firstWhere('id', $produto_aplicacao_f->produto_id);
-                    @endphp
+                            @php
+                                $equipamento = $equipamentos->firstWhere('id', $produto_aplicacao_f->equipamento);
+                            @endphp
+                            <td>
+                                {{ $equipamento ? $equipamento->nome : 'Equipamento não encontrado' }}
+                            </td>
 
-                    @if($produto)
-                    <td>{{ $produto->nome }}</td>
-                    @else
-                    <td class="not-found">Produto não encontrado</td>
-                    @endif
-
-                    @php
-                    $equipamento = $equipamentos->firstWhere('id', $produto_aplicacao_f->equipamento);
-                    @endphp
-
-                    @if($equipamento)
-                    <td>{{ $equipamento->nome }}</td>
-                    @else
-                    <td class="not-found">Equipamento não encontrado</td>
-                    @endif
-                    <td>{{ $produto_aplicacao_f->quantidade }}</td>
-                    <td>{{ $produto_aplicacao_f->descricao }}</td>
-                    <td>{{ $produto_aplicacao_f->horas_proxima_manutencao }}</td>
-                    <td>{{ $produto_aplicacao_f->data_proxima_manutencao }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+                            <td class="text-center">{{ $produto_aplicacao_f->quantidade }}</td>
+                            <td>{{ $produto_aplicacao_f->descricao }}</td>
+                            <td class="text-center">{{ $produto_aplicacao_f->horas_proxima_manutencao }}</td>
+                            <td class="text-center">{{ $produto_aplicacao_f->data_proxima_manutencao }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 </main>
