c2bf772c8df3120e0c2a8d11face5d062715f128
Sidinei Da Rosa <70112853+SidineiDaRosa@users.noreply.github.com>
2026-01-09 17:03:51 -0300

    up

--- resources/views/app/ordem_servico/panel_os.blade.php
+++ resources/views/app/ordem_servico/panel_os.blade.php
@@ -2,6 +2,7 @@
 <html lang="pt-BR">
 <head>
 <meta charset="UTF-8">
+<meta http-equiv="refresh" content="60">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Painel OS Grid</title>
 
