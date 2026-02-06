fde7a806616f18cc6c9662b9821a084dd25fa5cc
Sidinei Da Rosa <70112853+SidineiDaRosa@users.noreply.github.com>
2026-02-06 10:22:01 -0300

    up

--- resources/views/app/meu-pdf.blade.php
+++ resources/views/app/meu-pdf.blade.php
@@ -284,24 +284,46 @@ Descrição dos serviços executados:
     }
 </style>
 <?php
-// Imagem da Solicitação (já estava certa)
+// ===== IMAGEM DA SOLICITAÇÃO =====
 if (!empty($solicitacao_os->imagem)) {
-    echo '<img src="' . public_path('img/request_os/' . $solicitacao_os->imagem) . '" class="img-ajustada">';
-}
 
-// Imagem da OS (CORRIGIDO para public)
-if (!empty($ordemServico->link_foto)) {
-    $caminho = public_path($ordemServico->link_foto);
+    $caminhoReq = public_path('img/request_os/'.$solicitacao_os->imagem);
 
-    if (file_exists($caminho)) {
-        echo '<img src="' . $caminho . '" class="img-ajustada">';
+    if (file_exists($caminhoReq)) {
+        $tipoReq = pathinfo($caminhoReq, PATHINFO_EXTENSION);
+        $imgReqBase64 = base64_encode(file_get_contents($caminhoReq));
+
+        echo '<img src="data:image/'.$tipoReq.';base64,'.$imgReqBase64.'" class="img-ajustada">';
     } else {
-        echo '<p style="color:red">Imagem não encontrada: ' . $caminho . '</p>';
+        echo '<p style="color:red">Imagem da solicitação não encontrada.</p>';
     }
 }
+
+// ===== IMAGEM DA OS =====
+
 ?>
 
 
+<!--if (!empty($ordemServico->link_foto)) {
+
+    $caminho = public_path($ordemServico->link_foto);
+
+    if (file_exists($caminho)) {
+        $tipo = pathinfo($caminho, PATHINFO_EXTENSION);
+        $imagemBase64 = base64_encode(file_get_contents($caminho));
+
+        echo '<img src="data:image/'.$tipo.';base64,'.$imagemBase64.'" class="img-ajustada">';
+    } else {
+        echo '<p style="color:red">
+                Imagem não encontrada no caminho:<br>'.$caminho.'
+              </p>';
+    }
+
+} else {
+    echo '<p style="color:orange">
+            Nenhuma imagem vinculada a esta OS.
+          </p>';
+}-->
 </body>
 
 </html>
