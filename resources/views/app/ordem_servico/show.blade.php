1559cd00595ac62b02f406bcecad1accd904896f
Sidinei Da Rosa <70112853+SidineiDaRosa@users.noreply.github.com>
2026-02-18 08:07:35 -0300

    up

--- resources/views/app/ordem_servico/show.blade.php
+++ resources/views/app/ordem_servico/show.blade.php
@@ -1087,3 +1087,32 @@
         font-weight: 600;
     }
 </style>
+
+<script>
+    //Fechar ordem de serviço pelo botão finalizar
+    $(document).ready(function() {
+        $('#confirmarEnvio').click(function() {
+          
+            var valor = $('#valor').val(); // Obtém o valor do input
+
+            $.ajax({
+                type: 'GET', // Método HTTP da requisição
+                url: '{{ route('updateos') }}', // URL para onde a requisição será enviada
+                data: {
+                    valor: valor
+                }, // Dados a serem enviados (no formato chave: valor)
+                success: function(response) {
+                    $('#mensagem').text('Resposta do servidor: ' +
+                        response); // Exibe a resposta do servidor
+                    $('#sucessoModal').modal('show'); // Exibe a modal de sucesso
+                },
+                error: function(xhr, status, error) {
+                    $('#mensagem').text('Erro ao enviar valor: ' +
+                        error); // Exibe mensagem de erro, se houver
+                    $('#erroModal').modal('show'); // Exibe a modal de sucesso
+                }
+            });
+        });
+    });
+</script>
+
