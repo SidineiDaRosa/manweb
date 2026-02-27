7e9651310696e47baa6d4159662ec95b4e5db148
Carolbuenodarosa <seuemaildopgithub@gmail.com>
2026-02-26 21:20:38 -0300

    up

--- resources/views/app/ordem_servico/show.blade.php
+++ resources/views/app/ordem_servico/show.blade.php
@@ -331,8 +331,7 @@
                             </div>
                             <hr>
                             <!--Progressbar com um input texto-->
-                            <div class="titulo" style="font-size:15px;font-weight:300;">Progresso:</div>
-                            <div class="progress" style="height: 5px">
+                            <div class="progress" style="height:10px">
                                 <div id="progress-bar" class="progress-bar" role="progressbar" aria-valuenow="0"
                                     aria-valuemin="0" aria-valuemax="100">{{ $ordem_servico->status_servicos }}%</div>
                             </div>
