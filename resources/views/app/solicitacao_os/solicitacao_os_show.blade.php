29898ebbda79ab2da50743cdf376e2dd7884981e
Sidinei Da Rosa <70112853+SidineiDaRosa@users.noreply.github.com>
2026-03-03 14:13:32 -0300

    up

--- resources/views/app/solicitacao_os/solicitacao_os_show.blade.php
+++ resources/views/app/solicitacao_os/solicitacao_os_show.blade.php
@@ -2,48 +2,43 @@
 @section('content')
 
 <main class="content">
-    <div class="titulo-main">
-        Solicitações de O.S
-        <a class="btn btn-outline-dark mb-1" href="{{ route('app.home') }}" style="width:200px;">
+    <h3 class="h3-gray">Soliciatação OS</h3>
+    <div class="header-grid">
+        <a class="btn-inf btn-inf-md btn-inf-brown" href="{{ route('app.home') }}" style="width:200px;">
             <i class="icofont-dashboard"></i> dashboard
         </a>
     </div>
-    <div class="div-datas">
-           Selecione o periodo
-        <style>
-            .div-datas {
-                margin-left: 20px;
-            }
-        </style>
-        <form action="{{ route('solicitacoes-os') }}" method="get" style="font-family: Arial,sans-serif;">
-            <input type="datetime-local" class="form-control" name="datetime" id="datetime" style="width:250px;"> <br>
-            <input type="datetime-local" class="form-control" name="datetime_fim" id="datetime_fim" style="width:250px;"> <br>
-            <label for="option-all">Todas</label>
-            <input type="radio" id="option-all" name="options" value="Todas" checked>
+    </style>
+    <form action="{{ route('solicitacoes-os') }}" method="get" style="font-family: Arial,sans-serif;">
+        <div class="header-grid">
+            <input type="datetime-local" class="input" name="datetime" id="datetime" style="width:200px;" required>
+            <input type="datetime-local" class="input" name="datetime_fim" id="datetime_fim" style="width:200px;" required>
+        </div>
+        <label for="option-all" class="btn-inf btn-inf-sm btn-inf-gray">Todas</label>
+        <input type="radio" id="option-all" name="options" value="Todas" checked>
 
-            <label for="option-accepted">Aceitas</label>
-            <input type="radio" id="option-accepted" name="options" value="Aceita">
+        <label for="option-accepted" class="btn-inf btn-inf-sm btn-inf-green"><i class="bi bi-check2-circle"></i> Aceitas</label>
+        <input type="radio" id="option-accepted" name="options" value="Aceita">
 
-            <label for="option-pending">Em espera</label>
-            <input type="radio" id="option-pending" name="options" value="Em Espera">
+        <label for="option-pending" class="btn-inf btn-inf-sm btn-inf-warning"><i class="bi bi-pause-circle"></i> Em espera</label>
+        <input type="radio" id="option-pending" name="options" value="Em Espera">
 
-            <label for="option-rejected">Recusada</label>
-            <input type="radio" id="option-rejected" name="options" value="Recusada">
-            <input type="hidden" name="receptor" value="{{ auth()->user()->nome }}">
-            <button type="submit" class="btn btn-outline-primary mb-1">
-                Buscar
-            </button>
+        <label for="option-rejected" class="btn-inf btn-inf-sm btn-inf-red"><i class="bi bi-x-circle-fill text-danger"></i> Recusada</label>
+        <input type="radio" id="option-rejected" name="options" value="Recusada">
+        <input type="hidden" name="receptor" value="{{ auth()->user()->nome }}">
+        <button type="submit" class="btn-inf btn-inf-md btn-inf-blue-dark">
+            <i class="bi bi-search"></i>
+            Buscar
+        </button>
 
-        </form>
-    </div>
-
+    </form>
     <style>
         .titulo-main {
             font-size: 20px;
             color: gray;
             text-align: center;
             margin-top: -2;
-            
+
         }
     </style>
     @foreach($solicitacoes as $solicitacao)
@@ -79,9 +74,9 @@ padding:5px;width:200px;
             Status: {{$solicitacao->status}} <br>
         </div> <br>
         <!-- Botão para "Aceitar" -->
-        <a class="btn btn-outline-primary mb-1" href="{{ route('solicitacao_os.aceitar', $solicitacao->id) }}"
+        <a class="btn-inf btn-inf-sm btn-inf-blue-dark" href="{{ route('solicitacao_os.aceitar', $solicitacao->id) }}"
             onclick="event.preventDefault(); document.getElementById('aceitar-form-{{ $solicitacao->id }}').submit();">
-            Aceitar
+            <i class="bi bi-check2-circle"></i> Aceitar
         </a>
 
         <form id="aceitar-form-{{ $solicitacao->id }}" action="{{ route('solicitacao_os.aceitar', $solicitacao->id) }}" method="POST" style="display: none;">
@@ -89,9 +84,9 @@ padding:5px;width:200px;
         </form>
 
         <!-- Botão para "Colocar em Espera" -->
-        <a class="btn btn-outline-warning mb-1" href="{{ route('solicitacao_os.espera', $solicitacao->id) }}"
+        <a class="btn-inf btn-inf-sm btn-inf-warning" href="{{ route('solicitacao_os.espera', $solicitacao->id) }}"
             onclick="event.preventDefault(); document.getElementById('espera-form-{{ $solicitacao->id }}').submit();">
-            Colocar em Espera
+          <i class="bi bi-pause-circle"></i> Colocar em Espera
         </a>
 
         <form id="espera-form-{{ $solicitacao->id }}" action="{{ route('solicitacao_os.espera', $solicitacao->id) }}" method="POST" style="display: none;">
@@ -99,9 +94,9 @@ padding:5px;width:200px;
         </form>
 
         <!-- Botão para "Recusar" -->
-        <a class="btn btn-outline-danger mb-1" href="{{ route('solicitacao_os.recusar', $solicitacao->id) }}"
+        <a class="btn-inf btn-inf-sm btn-inf-red" href="{{ route('solicitacao_os.recusar', $solicitacao->id) }}"
             onclick="event.preventDefault(); document.getElementById('recusar-form-{{ $solicitacao->id }}').submit();">
-            Recusar
+           <i class="bi bi-x-circle-fill text-danger"></i> Recusar
         </a>
 
         <form id="recusar-form-{{ $solicitacao->id }}" action="{{ route('solicitacao_os.recusar', $solicitacao->id) }}" method="POST" style="display: none;">
